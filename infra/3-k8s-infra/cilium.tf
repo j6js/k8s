c9b4a8cad489ef43c6f24f0c0baa1464601aae84
j6js <jackson@j6js.com>
2026-05-12 12:59:49 +1200

    refactor: remove externalCloudProvider configuration and update cilium gateway settings

--- infra/3-k8s-infra/cilium.tf
+++ infra/3-k8s-infra/cilium.tf
@@ -30,6 +30,10 @@ resource "helm_release" "cilium" {
       value = "true"
     },
     {
+      name  = "gatewayAPI.deployment.kind"
+      value = "DaemonSet"
+    },
+    {
       name = "securityContext.capabilities.ciliumAgent"
       value = "{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}"
     },
