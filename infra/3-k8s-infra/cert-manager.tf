0ccb256641dff5e41f16d255143c55bc73a1e69b
j6js <jackson@j6js.com>
2026-05-14 09:25:56 +1200

    feat: hand api secret over to terraform

---
+++ infra/3-k8s-infra/cert-manager.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_namespace_v1" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_secret_v1" "cert_manager_cf_api" {
+  metadata {
+    name      = "cert-manager-cf-api"
+    namespace = "cert-manager"
+  }
+  data = {
+    api-key = data.sops_file.cloudflare.data["apiToken"]
+  }
+  depends_on = [
+    kubernetes_namespace_v1.cert_manager,
+  ]
+}
